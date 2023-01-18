4c2f495dc892e45e1cd825f5b1b03360492f01b5
mclean0328 <50640513+mclean0328@users.noreply.github.com>
2023-01-18 16:19:22 -0500

    Terraform EKS w/ EBS CSI Add On (#53)
    * terraform example to create eks cluster and apply ebs csi add on which is necessary for eks cluster to use persistent volumes as of 1.23
    
    * change instance type to t3.medium

---
+++ terraform/aws/eks-ebs-csi-addon/variables.tf
@@ -0,0 +1,40 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "vpc_name" {
+  type = string
+  default = "example-vpc"
+}
+
+variable "cluster_name" {
+  type = string
+  default = "example-cluster"
+}
+
+variable "cluster_version" {
+  type = string
+  default = "1.23"
+}
+
+variable "node_group_one_name" {
+  type = string
+  default = "ng-example-1"
+}
+
+#variable "node_group_two_name" {
+# type = string
+#  default = "ng-example-2"
+#}
+
+variable "instance_type_one" {
+  type = string
+  default = "t3.medium"
+}
+
+#variable "instance_type_two" {
+#  type = string
+#  default = "t3.medium"
+#}
