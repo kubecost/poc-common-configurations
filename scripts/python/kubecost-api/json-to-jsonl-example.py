17f0665b1fc2b34835af9b2d3615a4a6d76b3ba5
mclean0328 <50640513+mclean0328@users.noreply.github.com>
2023-02-07 10:26:27 -0500

    Python Scripts (#57)
    * add a few scripts as examples for how to interact with different api endpoints
    
    * remove kubecost demo endpoint
    
    * Add README.md for the top level scripts directory and the kubecost-api sub-directory with overview information
    
    ---------
    
    Co-authored-by: mclean0328 <chris@chriss-MacBook-Pro.local>
    Co-authored-by: Sean Pomeroy <sean.pomeroy@gmail.com>

---
+++ scripts/python/kubecost-api/json-to-jsonl-example.py
@@ -0,0 +1,25 @@
+# This is a simple python script written to interact with the kubecost 'model/allocation' API endpoint.  The script will create a file json and jsonl file.
+# This script was intended to create jsonl output to be consumed by products like BigQuery which require jsonl
+# Please find more information about the 'model/allocation/' endpoint at https://docs.kubecost.com/apis/apis-overview/allocation
+
+import requests
+import json
+import jsonlines
+from datetime import datetime
+
+requestURL = "{insert request URL here}"
+
+# get datetime
+current_datetime = datetime.now()
+str_current_datetime = str(current_datetime)
+
+response = requests.get(requestURL).json()
+results = [v for object in response['data'] for v in object.values()]
+
+pretty = json.dumps(results, indent=4)
+
+with open("response"+str_current_datetime+".json", "w") as outfile:
+    outfile.write(pretty)
+
+with jsonlines.open("response"+str_current_datetime+".jl", 'w') as writer:
+    writer.write_all(results)
