ddaad53b3805436545213996b9891f8ba1b736b5
Jesse Goodier <31039225+jessegoodier@users.noreply.github.com>
2023-03-17 14:07:23 -0400

    ksm dedupe (#69)
    * ksm dedupe

--- custom-tsdb/kubecost-prometheus-healthcheck.sh
+++ custom-tsdb/kubecost-prometheus-healthcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# set hostname to prometheus endpoint.
-# if you don't see "missing test1" the script output is invalid.
+# set HOST_NAME to prometheus endpoint.
+# if you don't see "missing missing" the script output is invalid.
 
 export HOST_NAME="http://localhost:9090"
 
@@ -12,53 +12,59 @@ if  ! curl -s --data-urlencode "query=$1" "$HOST_NAME/api/v1/query"  |jq -r '.da
 fi
 }
 
-myArray=('test1
-avg(kube_pod_container_status_running{})by(pod,namespace)
-avg(kube_pod_container_status_running{})by(pod,namespace,uid)
-avg(avg_over_time(container_memory_allocation_bytes{container!="",container!="POD",node!=""}[5m]))by(container,pod,namespace,node,provider_id)
-avg(avg_over_time(kube_pod_container_resource_requests{resource="memory",unit="byte",container!="",container!="POD",node!=""}[5m]))by(container,pod,namespace,node)
-avg(avg_over_time(container_memory_working_set_bytes{container!="",container_name!="POD",container!="POD"}[5m]))by(container_name,container,pod_name,pod,namespace,instance)
-max(max_over_time(container_memory_working_set_bytes{container!="",container_name!="POD",container!="POD"}[5m]))by(container_name,container,pod_name,pod,namespace,instance)
-avg(avg_over_time(container_cpu_allocation{container!="",container!="POD",node!=""}[5m]))by(container,pod,namespace,node)
-avg(avg_over_time(kube_pod_container_resource_requests{resource="cpu",unit="core",container!="",container!="POD",node!=""}[5m]))by(container,pod,namespace,node)
-avg(rate(container_cpu_usage_seconds_total{container!="",container_name!="POD",container!="POD"}[5m]))by(container_name,container,pod_name,pod,namespace,instance)
-max(rate(container_cpu_usage_seconds_total{container!="",container_name!="POD",container!="POD"}[5m]))by(container_name,container,pod_name,pod,namespace,instance)
-avg(avg_over_time(container_gpu_allocation{container!="",container!="POD",node!=""}[5m]))by(container,pod,namespace,node)
-avg(avg_over_time(node_cpu_hourly_cost[5m]))by(node,instance_type,provider_id)
-avg(avg_over_time(node_ram_hourly_cost[5m]))by(node,instance_type,provider_id)
-avg(avg_over_time(node_gpu_hourly_cost[5m]))by(node,instance_type,provider_id)
-avg_over_time(kubecost_node_is_spot[5m])
-avg(kube_persistentvolumeclaim_info{volumename!=""})by(persistentvolumeclaim,storageclass,volumename,namespace)
-avg(avg_over_time(pod_pvc_allocation[5m]))by(persistentvolume,persistentvolumeclaim,pod,namespace)
-avg(avg_over_time(kube_persistentvolumeclaim_resource_requests_storage_bytes{}[5m]))by(persistentvolumeclaim,namespace)
-count(kube_persistentvolume_capacity_bytes)by(persistentvolume)
-avg(avg_over_time(kube_persistentvolume_capacity_bytes[5m]))by(persistentvolume)
-avg(avg_over_time(pv_hourly_cost[5m]))by(volumename)
-avg(avg_over_time(kubecost_network_zone_egress_cost{}[5m]))
-avg(avg_over_time(kubecost_network_region_egress_cost{}[5m]))
-avg(avg_over_time(kubecost_network_internet_egress_cost{}[5m]))
-sum(increase(container_network_receive_bytes_total{pod!=""}[5m]))by(pod_name,pod,namespace)
-sum(increase(container_network_transmit_bytes_total{pod!=""}[5m]))by(pod_name,pod,namespace)
-avg_over_time(kube_node_labels[5m])
-avg_over_time(kube_namespace_labels[5m])
-avg_over_time(kube_namespace_annotations[5m])
-avg_over_time(kube_pod_labels[5m])
-avg_over_time(kube_pod_annotations[5m])
-avg_over_time(service_selector_labels[5m])
-avg_over_time(deployment_match_labels[5m])
-sum(avg_over_time(kube_pod_owner{owner_kind="DaemonSet"}[5m]))by(pod,owner_name,namespace)
-sum(avg_over_time(kube_pod_owner{owner_kind="ReplicaSet"}[5m]))by(pod,owner_name,namespace)
-kube_node_status_capacity
-kube_node_status_capacity_memory_bytes
-kube_node_status_capacity_cpu_cores
-kube_node_status_allocatable
-kube_node_status_allocatable_memory_bytes
-kube_node_status_allocatable_cpu_cores
-kube_node_labels
-kube_node_status_condition
+myArray=('missing
+container_cpu_allocation
+container_cpu_usage_seconds_total
+container_gpu_allocation
+container_memory_allocation_bytes
+container_memory_working_set_bytes
+container_network_receive_bytes_total
+container_network_transmit_bytes_total
+deployment_match_labels
 kube_namespace_labels
+kube_node_labels
+kube_node_status_allocatable
+kube_node_status_allocatable_cpu_cores
+kube_node_status_allocatable_memory_bytes
+kube_node_status_capacity
+kube_node_status_capacity_cpu_cores
+kube_node_status_capacity_memory_bytes
+kube_node_status_condition
+kube_persistentvolume_capacity_bytes
+kube_persistentvolumeclaim_resource_requests_storage_bytes
+kube_pod_container_resource_requests
+kube_pod_container_status_running
 kube_pod_labels
-kube_pod_owner')
+kube_pod_owner
+kubecost_allocation_data_status
+kubecost_asset_data_status
+kubecost_cluster_info
+kubecost_cluster_management_cost
+kubecost_etl_events_total
+kubecost_etl_progress_percent
+kubecost_load_balancer_cost
+kubecost_network_internet_egress_cost
+kubecost_network_region_egress_cost
+kubecost_network_zone_egress_cost
+kubecost_pod_network_egress_bytes_total
+kubecost_pod_network_ingress_bytes_total
+kubecost_node_is_spot
+node_cpu_hourly_cost
+node_gpu_count
+node_gpu_hourly_cost
+node_ram_hourly_cost
+node_total_hourly_cost
+pod_pvc_allocation
+process_cpu_seconds_total
+process_max_fds
+process_open_fds
+process_resident_memory_bytes
+process_start_time_seconds
+process_virtual_memory_bytes
+process_virtual_memory_max_bytes
+pv_hourly_cost
+service_selector_labels
+statefulSet_match_labels')
 
 for str in ${myArray[@]}; do
 check_metric $str
