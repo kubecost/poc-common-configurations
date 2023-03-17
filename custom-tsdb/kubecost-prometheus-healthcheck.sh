c076280d31521dab550bad53c7333b48f2cd698f
jesse goodier <31039225+jessegoodier@users.noreply.github.com>
2023-03-17 08:32:21 -0400

    ksm dedupe

--- custom-tsdb/kubecost-prometheus-healthcheck.sh
+++ custom-tsdb/kubecost-prometheus-healthcheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# set hostname to prometheus endpoint.
-# if you don't see "missing test1" the script output is invalid.
+# set HOST_NAME to prometheus endpoint.
+# if you don't see "missing missing" the script output is invalid.
 
 export HOST_NAME="http://localhost:9090"
 
@@ -12,38 +12,48 @@ if  ! curl -s --data-urlencode "query=$1" "$HOST_NAME/api/v1/query"  |jq -r '.da
 fi
 }
 
-myArray=('container_cpu_allocation
-container_cpu_usage_seconds_total
+myArray=('missing
+container_cpu_allocation
 container_gpu_allocation
 container_memory_allocation_bytes
-container_memory_working_set_bytes
-container_network_receive_bytes_total
-container_network_transmit_bytes_total
-kube_namespace_labels
-kube_node_labels
-kube_node_status_allocatable
+deployment_match_labels
 kube_node_status_allocatable_cpu_cores
 kube_node_status_allocatable_memory_bytes
-kube_node_status_capacity
 kube_node_status_capacity_cpu_cores
 kube_node_status_capacity_memory_bytes
-kube_node_status_condition
-kube_persistentvolume_capacity_bytes
-kube_persistentvolumeclaim_resource_requests_storage_bytes
-kube_pod_container_resource_requests
-kube_pod_container_status_running
-kube_pod_labels
-kube_pod_owner
+kubecost_allocation_data_status
+kubecost_asset_data_status
+kubecost_cluster_info
+kubecost_cluster_management_cost
+kubecost_etl_events_total
+kubecost_etl_progress_percent
+kubecost_http_requests_total
+kubecost_http_response_size_bytes_count
+kubecost_http_response_size_bytes_sum
+kubecost_http_response_time_seconds_bucket
+kubecost_http_response_time_seconds_count
+kubecost_http_response_time_seconds_sum
+kubecost_load_balancer_cost
 kubecost_network_internet_egress_cost
 kubecost_network_region_egress_cost
 kubecost_network_zone_egress_cost
 kubecost_node_is_spot
 node_cpu_hourly_cost
+node_gpu_count
 node_gpu_hourly_cost
 node_ram_hourly_cost
+node_total_hourly_cost
 pod_pvc_allocation
+process_cpu_seconds_total
+process_max_fds
+process_open_fds
+process_resident_memory_bytes
+process_start_time_seconds
+process_virtual_memory_bytes
+process_virtual_memory_max_bytes
 pv_hourly_cost
-test1
+service_selector_labels
+statefulSet_match_labels
 ')
 
 for str in ${myArray[@]}; do
