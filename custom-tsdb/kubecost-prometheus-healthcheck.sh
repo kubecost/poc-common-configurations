2879a2e9742beff640492df96e150d1471fec77f
jesse goodier <31039225+jessegoodier@users.noreply.github.com>
2023-03-20 14:20:43 -0400

    added metrics

--- custom-tsdb/kubecost-prometheus-healthcheck.sh
+++ custom-tsdb/kubecost-prometheus-healthcheck.sh
@@ -2,6 +2,7 @@
 
 # set HOST_NAME to prometheus endpoint.
 # if you don't see "missing missing" the script output is invalid.
+# some metrics are optional: kubecost_pod_network* and GPU metrics
 
 export HOST_NAME="http://localhost:9090"
 
@@ -15,12 +16,15 @@ fi
 myArray=('missing
 container_cpu_allocation
 container_cpu_usage_seconds_total
+container_fs_limit_bytes
+container_fs_usage_bytes
 container_gpu_allocation
 container_memory_allocation_bytes
 container_memory_working_set_bytes
 container_network_receive_bytes_total
 container_network_transmit_bytes_total
 deployment_match_labels
+kube_namespace_annotations
 kube_namespace_labels
 kube_node_labels
 kube_node_status_allocatable
@@ -31,11 +35,14 @@ kube_node_status_capacity_cpu_cores
 kube_node_status_capacity_memory_bytes
 kube_node_status_condition
 kube_persistentvolume_capacity_bytes
+kube_persistentvolumeclaim_info
 kube_persistentvolumeclaim_resource_requests_storage_bytes
+kube_pod_annotations
 kube_pod_container_resource_requests
 kube_pod_container_status_running
 kube_pod_labels
 kube_pod_owner
+kube_replicaset_owner
 kubecost_allocation_data_status
 kubecost_asset_data_status
 kubecost_cluster_info
@@ -46,10 +53,13 @@ kubecost_load_balancer_cost
 kubecost_network_internet_egress_cost
 kubecost_network_region_egress_cost
 kubecost_network_zone_egress_cost
+kubecost_node_is_spot
 kubecost_pod_network_egress_bytes_total
 kubecost_pod_network_ingress_bytes_total
-kubecost_node_is_spot
+kubecost_pv_info
+kubelet_volume_stats_used_bytes
 node_cpu_hourly_cost
+node_cpu_seconds_total
 node_gpu_count
 node_gpu_hourly_cost
 node_ram_hourly_cost
