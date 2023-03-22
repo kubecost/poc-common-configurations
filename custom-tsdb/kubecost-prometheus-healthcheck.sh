2924e874229c85f4b9efca0d789b71fd6ddfa6a8
jesse goodier <31039225+jessegoodier@users.noreply.github.com>
2023-03-22 15:49:43 -0400

    metrics checker

--- custom-tsdb/kubecost-prometheus-healthcheck.sh
+++ custom-tsdb/kubecost-prometheus-healthcheck.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
 # set HOST_NAME to prometheus endpoint.
-# if you don't see "missing missing" the script output is invalid.
-# some metrics are optional: kubecost_pod_network* and GPU metrics
+# there is a false negative test, if you don't see "FALSE_NEGATIVE" in the the OPTIONAL_METRICS, script output is invalid.
+# some metrics are optional: kubecost_pod_network*/GPU metrics/container_fs
 
 export HOST_NAME="http://localhost:9090"
 
 check_metric()
 {
-if  ! curl -s --data-urlencode "query=$1" "$HOST_NAME/api/v1/query"  |jq -r '.data.result[].value[]' --exit-status > /dev/null  ; then
- echo "missing" $1
+if  curl -s --data-urlencode "query=absent_over_time($1[5m])" "$HOST_NAME/api/v1/query"  |jq -r '.data.result[].value[]' --exit-status > /dev/null  ; then
+ echo $1
 fi
 }
 
-myArray=('missing
-container_cpu_allocation
+REQUIRED_METRICS=('container_cpu_allocation
 container_cpu_usage_seconds_total
+kube_node_status_allocatable
+kube_node_status_allocatable_cpu_cores
+kube_node_status_allocatable_memory_bytes
+kube_node_status_capacity
+kube_node_status_capacity_cpu_cores
+kube_node_status_capacity_memory_bytes
+kube_pod_container_resource_requests
+kube_pod_container_status_running
+node_cpu_hourly_cost
+node_ram_hourly_cost
+node_total_hourly_cost')
+
+OPTIONAL_METRICS=('FALSE_NEGATIVE
 container_fs_limit_bytes
 container_fs_usage_bytes
 container_gpu_allocation
@@ -27,19 +39,11 @@ deployment_match_labels
 kube_namespace_annotations
 kube_namespace_labels
 kube_node_labels
-kube_node_status_allocatable
-kube_node_status_allocatable_cpu_cores
-kube_node_status_allocatable_memory_bytes
-kube_node_status_capacity
-kube_node_status_capacity_cpu_cores
-kube_node_status_capacity_memory_bytes
 kube_node_status_condition
 kube_persistentvolume_capacity_bytes
 kube_persistentvolumeclaim_info
 kube_persistentvolumeclaim_resource_requests_storage_bytes
 kube_pod_annotations
-kube_pod_container_resource_requests
-kube_pod_container_status_running
 kube_pod_labels
 kube_pod_owner
 kube_replicaset_owner
@@ -58,12 +62,9 @@ kubecost_pod_network_egress_bytes_total
 kubecost_pod_network_ingress_bytes_total
 kubecost_pv_info
 kubelet_volume_stats_used_bytes
-node_cpu_hourly_cost
 node_cpu_seconds_total
 node_gpu_count
 node_gpu_hourly_cost
-node_ram_hourly_cost
-node_total_hourly_cost
 pod_pvc_allocation
 process_cpu_seconds_total
 process_max_fds
@@ -76,6 +77,12 @@ pv_hourly_cost
 service_selector_labels
 statefulSet_match_labels')
 
-for str in ${myArray[@]}; do
+echo "REQUIRED_METRICS Missing:"
+for str in ${REQUIRED_METRICS[@]}; do
+check_metric $str
+done
+
+echo "OPTIONAL_METRICS Missing:"
+for str in ${OPTIONAL_METRICS[@]}; do
 check_metric $str
 done
